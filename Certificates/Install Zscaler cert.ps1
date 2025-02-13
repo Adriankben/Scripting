7204fb20eb84264ad96fbe4eb0240779c487bee1
Adrian Bennett <34956924+Adriankben@users.noreply.github.com>
2025-02-13 08:25:32 -0500

    Updates to scripts

---
+++ Certificates/Install Zscaler cert.ps1
@@ -0,0 +1,5 @@
+﻿
+$CertPath = "C:\Users\abennett\OneDrive - GA Telesis, LLC\Documents\Cert\Zscaler.crt"
+$CertStore = "Cert:\LocalMachine\Root"  # Change if needed (e.g., "Cert:\LocalMachine\My")
+
+Import-Certificate -FilePath $CertPath -CertStoreLocation $CertStore
---
+++ Proxy/Enable proxy url.ps1
@@ -0,0 +1,27 @@
+﻿# Define the PAC script URL
+$pacUrl = "http://proxyserver/proxy.pac"
+
+# Enable Proxy Auto Config script
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+Set-ItemProperty -Path $registryPath -Name "AutoConfigURL" -Value $pacUrl
+
+# Notify the system about the changes
+$refresh = New-Object -ComObject Shell.Application
+$refresh.Windows()
+
+Write-Output "Proxy setup script enabled: $pacUrl"
+
+
+Get-ChildItem "C:\Users" -Force | ForEach-Object {
+    $userRegPath = "C:\Users\$_\NTUSER.DAT"
+    if (Test-Path $userRegPath) {
+        reg load HKU\TempUser $userRegPath
+        Set-ItemProperty -Path "HKU:\TempUser\Software\Microsoft\Windows\CurrentVersion\Internet Settings" -Name AutoConfigURL -Value $pacUrl
+        reg unload HKU\TempUser
+    }
+}
+
+
+
+
+
